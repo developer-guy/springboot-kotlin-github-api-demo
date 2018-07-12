6a96f25b1a30078a21f16578d3e6138eee2c7de1
Batuhan Apaydın <batuhan.apaydin@trendyol.com>
2018-07-12 08:39:50 +0300

    review

--- src/main/kotlin/com/github/springbootkotlingithubapi/model/RepositoryEvent.kt
+++ src/main/kotlin/com/github/springbootkotlingithubapi/model/RepositoryEvent.kt
@@ -3,7 +3,8 @@ package com.github.springbootkotlingithubapi.model
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import java.time.OffsetDateTime
-//@JsonCreator is optional !!
+
+//@JsonCreator is optional unless secondary constructor available!!
 data class RepositoryEvent(@JsonProperty("event") @JsonDeserialize(using = EventToTypeDeSerializer::class) val type: Type,
                            @JsonProperty("created_at") val creationTime: OffsetDateTime,
                            @JsonProperty("actor") val actor: Actor,
